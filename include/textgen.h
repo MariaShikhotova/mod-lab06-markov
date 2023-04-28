36b9b4778aa896e15630aacc1f499691b5f62f58
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-04-28 13:09:08 +0300

    Create textgen.h

---
+++ include/textgen.h
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+class text {
+ public:
+  void read(std::string filename,
+            std::map<std::string, std::vector<std::string>>& markovChain,
+            int prefixLength);
+  void generate(std::map<std::string, std::vector<std::string>> markovChain,
+                int prefixLength, int textLength);
+  std::vector<std::string> prefix;
+  std::string stop;
+  std::string generateText;
+};
